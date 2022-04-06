92d35d93e9dc403b901d82c7845fb2b8a47b3875
Bipro <biprobarai18@gmail.com>
2022-04-06 08:25:23 +0600

    dynamic-route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import {
   Route,
 } from "react-router-dom";
 import './App.css';
+
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+
+
 import AuthProvider from "./contexts/Authprovider/AuthProvider";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import Home from "./Pages/Home/Home/Home";
@@ -16,9 +26,31 @@ import Contact from "./Pages/More/Contact/Contact";
 import More from "./Pages/More/More/More";
 import Take from "./Pages/More/Take/Take";
 
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
 function App() {
+
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
   return (
     <div className="App">
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          bgcolor: 'background.default',
+          color: 'text.primary',
+          borderRadius: 1,
+          p: 3,
+        }}
+      >
+        {theme.palette.mode} mode
+        <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+          {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+      </Box>
       <AuthProvider>
         <BrowserRouter>
           <Routes>
--- src/Pages/More/Take/Take.js
+++ src/Pages/More/Take/Take.js
@@ -1,16 +1,75 @@
-import React from 'react';
+import { Grid, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
+import useAuth from '../../../Hooks/useAuth';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 
 const Take = () => {
+    const { user } = useAuth();
+    let { serviceId } = useParams();
+    const [details, setDetails] = useState([]);
+    const [BookDetails, setBookDetails] = useState({});
+    useEffect(() => {
+        fetch('/book.json')
+            .then(res => res.json())
+            .then(data => setDetails(data));
+    }, [])
+
+    // console.log(details);
+    // const b = details.filter(sv => sv.id === serviceId);
+    // console.log(b[0]);
+    // const found = details.filter(serv => serv.id === serviceId);
+    useEffect(() => {
+        const foundBook = details.find(
+            serv => serv.id === serviceId
+        );
+        setBookDetails(foundBook);
+    }, [details, serviceId])
+    console.log(BookDetails);
     return (
-        <div>
-            <Header />
-            <h3>welcome monu</h3>
+        <>
+            <Header></Header>
+            <div className="mx-auto">
+
+                <Grid sx={{ alignItems: 'center' }}>
+                    <Typography>
+                        <img src={user.photoURL} alt="" />
+                        <h2 style={{ color: 'white' }}>Hey "{user?.displayName}" Welcome</h2>
+                    </Typography>
+                    <Typography sx={{ mx: 'auto' }}>
+                        <img style={{ width: '50%' }} src={BookDetails?.thumb} alt="" />
+
+                    </Typography>
+                    <Typography style={{ margin: '0 50px 0 50px', alignItems: 'center' }}>
+                        Name: {BookDetails?.course} <br />
+                        details: {BookDetails?.description}
+                    </Typography>
+                </Grid>
+                <Container>
+                    <Col className="ms-5">
+                        <Card className="card h-100 w-50">
+                            <Card.Img style={{ height: '250px' }} variant="top" src={BookDetails?.thumb} />
+                            <Card.Body>
+                                <Card.Title>{BookDetails?.category}
+                                </Card.Title>
+                                <p> {BookDetails?.course}</p>
+                                <p> Price: {BookDetails?.price} $</p>
+                                <Link to="/enroll"> <button style={{ alignItems: 'center', marginLeft: "40px" }} className="btn btn-primary ms-40px">Enroll Now</button></Link>
+                            </Card.Body>
 
 
-            <Footer />
-        </div>
+
+                        </Card>
+
+
+                    </Col>
+                </Container>
+            </div>
+            <Footer></Footer>
+        </>
     );
 };
 
